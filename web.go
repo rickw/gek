b1e9a4a449d0092532be5020aca1302ffb2424de
Rick Windham <rick.windham@gmail.com>
2015-08-10 22:17:45 -0400

    add empy routers

--- web.go
+++ web.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.GET("/to_col_notation/:num", ToCol)
+	router.GET("/from_col_notation/:str", FromCol)
 
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
@@ -20,4 +22,14 @@ func main() {
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.ServeFile(w, r, "index.html")
+}
+
+func ToCol(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// ps.ByName("num")
+	fmt.Fprintf(w, "sorry, %s!\n", ps.ByName("num"))
+}
+
+func FromCol(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// ps.ByName("str")
+	fmt.Fprintf(w, "sorry, %s!\n", ps.ByName("str"))
 }
