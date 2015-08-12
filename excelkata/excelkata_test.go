18dc931b51f57a56ad7edc5a54a85566de40f165
Rick Windham <rick.windham@gmail.com>
2015-08-11 23:59:14 -0400

    next step

--- excelkata/excelkata_test.go
+++ excelkata/excelkata_test.go
@@ -1,5 +1,21 @@
 package excelkata
 
 import (
+	"net/http"
 	"testing"
 )
+
+const (
+	TO_URL   = "http://code-challenge.afitnerd.com/v1/excel_kata/to_col_notation/%i"
+	FROM_URL = "http://code-challenge.afitnerd.com/v1/excel_kata/from_col_notation/%s"
+)
+
+func toCol(number int) string {
+	req_url := sprintf(TO_URL, number)
+	resp, err := http.Get(req_url)
+}
+
+func fromCol(str string) int {
+	req_url := sprintf(FROM_URL, str)
+	resp, err := http.Get(req_url)
+}
